7ed0e3afa8089740737d3291cf1e8c6d85195509
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-02-24 20:18:31 +0200

    Add-Post-View-Validation

---
+++ Web/Unitial.Web/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unitial.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreatePost(string caption, string imageUrl)
+        {
+            return View();
+        }
+    }
+}
+
