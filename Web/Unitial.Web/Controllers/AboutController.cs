d7bbb3cf77277ece5b62af1708de657df91a0f9a
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-02-29 14:38:43 +0200

    About-Page-Added

---
+++ Web/Unitial.Web/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unitial.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        [HttpGet("/About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
