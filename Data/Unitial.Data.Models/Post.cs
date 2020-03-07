596408a3a9772e789e7b5121950270c3ae84ed56
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-03-07 14:02:04 +0200

    Post-Deletable

--- Data/Unitial.Data.Models/Post.cs
+++ Data/Unitial.Data.Models/Post.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unitial.Data.Common.Models;
 
 namespace Unitial.Data.Models
 {
-    public class Post
+    public class Post : IDeletableEntity
     {
         public Post()
         {
             this.Comments = new HashSet<Comment>();
             this.Likes = new HashSet<Like>();
             this.PostedOn = DateTime.UtcNow;
+            this.IsDeleted = false;
         }
         public string Id { get; set; }
 
@@ -22,12 +24,11 @@ namespace Unitial.Data.Models
         public DateTime PostedOn { get; set; }
         public bool HaveLikes { get; set; }
         public bool HaveComments { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
 
         public ICollection<Comment> Comments { get; set; }
         public ICollection<Like> Likes { get; set; }
-
-
-
     }
 }
 
---
+++ Data/Unitial.Data/Migrations/20200307114719_Post-Deletable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Unitial.Data.Migrations
+{
+    public partial class PostDeletable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Posts",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_IsDeleted",
+                table: "Posts",
+                column: "IsDeleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_IsDeleted",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Posts");
+        }
+    }
+}
