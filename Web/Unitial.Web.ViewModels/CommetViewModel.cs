aa074d63949628dcbad5b6623c34435da9e97222
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-03-26 13:46:54 +0200

    Add-Comment`s-Profile-Pics

---
+++ Web/Unitial.Web.ViewModels/CommetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unitial.Web.ViewModels
+{
+    public class CommetViewModel
+    {
+        public string Id { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string CreatorProfilePic { get; set; }
+
+        public DateTime CommentOn { get; set; }
+
+        public string CommentText { get; set; }
+
+        public string PostId { get; set; }
+    }
+}
