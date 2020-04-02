3890a603b3e04385821aee4409dfa14ecfed1253
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-04-02 16:40:18 +0300

    Started-Conversations-Are-Get-Correctly

---
+++ Services/Unitial.Services.Data/MessageService.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using Unitial.Data.Common.Repositories;
+using Unitial.Data.Models;
+using Unitial.Web.ViewModels.Message;
+
+namespace Unitial.Services.Data
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IRepository<Conversation> conversationRepository;
+        private readonly IRepository<ApplicationUser> userRepository;
+
+        public MessageService(IRepository<Conversation> conversationRepository, IRepository<ApplicationUser> userRepository)
+        {
+            this.conversationRepository = conversationRepository;
+            this.userRepository = userRepository;
+        }
+
+        public string CreateConversation(string senderId, string receiverId)
+        {
+            var firstUserExist = userRepository.All().Where(x => x.Id == senderId).Any();
+            var secondUserExist = userRepository.All().Where(x => x.Id == receiverId).Any();
+            if (firstUserExist && secondUserExist)
+            {
+                var conversation = new Conversation()
+                {
+                    FirstUserId = senderId,
+                    SecondUserId = receiverId,
+                    SeenFirstUser = true,
+                    SeenSecondUser = true,
+                };
+                conversationRepository.AddAsync(conversation);
+
+                conversationRepository.SaveChangesAsync().GetAwaiter().GetResult();
+                return conversation.Id;
+            }
+            return null;
+        }
+
+        public void DeleteConversation(string senderId, string receiverId)
+        {
+            var conversation = conversationRepository.All().Where(x =>
+              (x.FirstUserId == senderId && x.SecondUserId == receiverId) ||
+              (x.SecondUserId == senderId && x.FirstUserId == receiverId)).FirstOrDefault();
+
+            conversationRepository.Delete(conversation);
+
+            conversationRepository.SaveChangesAsync().GetAwaiter().GetResult();
+        }
+
+        public AllConversationsViewModel GetAllConversations(string userId)
+        {
+            var conversation = conversationRepository
+                .All()
+                .Where(x => x.SecondUserId == userId || x.FirstUserId == userId)
+                .Select(x => new ConversationViewModel()
+                {
+                    LastUpdate = x.LastUpdate,
+                    User = userRepository.All().Where(y => y.Id == (x.SecondUserId == userId ? x.FirstUserId : x.SecondUserId)).FirstOrDefault()
+                });
+            var allConversations = new AllConversationsViewModel() { Conversations = conversation };
+            return allConversations;
+        }
+
+        public bool IsCreated(string senderId, string receiverId)
+        {
+            var IsCreated = conversationRepository.All().Where(x =>
+              (x.FirstUserId == senderId && x.SecondUserId == receiverId) ||
+              (x.SecondUserId == senderId && x.FirstUserId == receiverId)).Any();
+
+            return IsCreated;
+        }
+    }
+}
--- Web/Unitial.Web/Startup.cs
+++ Web/Unitial.Web/Startup.cs
@@ -73,6 +73,8 @@
             services.AddTransient<ICommentService, CommentService>();
             services.AddTransient<ILikeService, LikeService>();
             services.AddTransient<IFollowService, FollowService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IMessageService, MessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
