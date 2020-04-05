2309f515af4fb28976d4c94026e028dce02a0c71
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-04-05 12:37:41 +0300

    Auto-Scroll-Messages

---
+++ Web/Unitial.Web.ViewModels/Message/AllConversationsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using Unitial.Data.Models;
+
+namespace Unitial.Web.ViewModels.Message
+{
+    public class AllConversationsViewModel
+    {
+        public IQueryable<ConversationViewModel> Conversations { get; set; }
+    }
+}
