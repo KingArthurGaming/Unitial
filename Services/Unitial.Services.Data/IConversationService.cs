be27aeee71988bac5802cb80f94d26be5a4e10fb
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-04-07 18:23:26 +0300

    Conversations-All-Design

---
+++ Services/Unitial.Services.Data/IConversationService.cs
@@ -0,0 +1,14 @@
+﻿using Unitial.Web.ViewModels.Message;
+
+namespace Unitial.Services.Data
+{
+    public interface IConversationService
+    {
+        public string CreateConversation(string senderId, string receiverId);
+        public void DeleteConversation(string senderId, string receiverId);
+        public bool IsCreated(string senderId, string receiverId);
+        public AllConversationsViewModel GetAllConversations(string senderId);
+        public string GetConversationId(string senderId, string receiverId);
+        public void SetLastUpdate(string conversationId);
+    }
+}
