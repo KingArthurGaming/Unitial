e44598691eed823d32226919230d87323a141b42
King Arthur <50965467+KingArthurGaming@users.noreply.github.com>
2020-04-15 12:30:51 +0300

    Check-For-New-Messages-Everywhere

---
+++ Unitial.Tests/HomeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Unitial.Services.Data;
+using Unitial.Web.Controllers;
+using Unitial.Web.ViewModels;
+using Xunit;
+
+namespace Unitial.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void TestViewModelForHomePage()
+        {
+            var controller = new HomeController(new MockPostService(), new MockUserService());
+        }
+
+
+
+        public class MockPostService : IPostService
+        {
+            public void CreatePost(CreatePostInputModel createPostInput, string userId)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void DeletePost(string postId)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public ICollection<PostViewModel> GetPostsById(string userId, string activeUserId)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        public class MockUserService : IUserService
+        {
+            public string GetUserByUsername(string Username)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
